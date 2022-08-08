011f29e02adbcadf7fc628920ad727b7de8afa8d
Patt-Mac <MaddHatt.pm@gmail.com>
2022-08-07 23:10:45 -0400

    Completed Project01 - WeSplitApp

---
+++ Project01-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Patt Martin on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
