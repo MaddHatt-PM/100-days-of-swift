eaff5709788e3b4e51f535804a94218d951b8f47
Patt-Mac <MaddHatt.pm@gmail.com>
2022-08-09 19:22:01 -0400

    Completed challenge: conditional modifer

--- Project01-WeSplit/WeSplit/ContentView.swift
+++ Project01-WeSplit/WeSplit/ContentView.swift
@@ -8,87 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 0
-    @State private var tipPercentage = 20
-    @FocusState private var amountIsFocused: Bool
+  @State private var checkAmount = 0.0
+  @State private var numberOfPeople = 0
+  @State private var tipPercentage = 20
+  @FocusState private var amountIsFocused: Bool
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
+  
+  var peopleTotal: Double {
+    let peopleCountOffset = 2
+    let peopleCount = Double(numberOfPeople + peopleCountOffset)
+    return peopleCount
+  }
+  
+  var grandTotal: Double {
+    let tipSelection = Double(tipPercentage) * 0.01
     
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
-    var peopleTotal: Double {
-        let peopleCountOffset = 2
-        let peopleCount = Double(numberOfPeople + peopleCountOffset)
-        return peopleCount
-    }
-    
-    var grandTotal: Double {
-        let tipSelection = Double(tipPercentage) * 0.01
-        
-        let tipValue = checkAmount * tipSelection
-        let grandTotal = checkAmount + tipValue
-        return grandTotal
-    }
-    
-    var totalPerPerson: Double {
-        let amountPerPerson = grandTotal / peopleTotal
-        return amountPerPerson
-    }
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    TextField("Amount", value: $checkAmount, format:
-                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            .keyboardType(.decimalPad)
-                            .focused($amountIsFocused)
-                    
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
-                            Text("\($0) people")
-                        }
-                    }
-                }
-                
-                Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                } header: {
-                    Text("How much tip do you want to leave?")
-                }
-                
-                Section {
-                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                } header: {
-                    Text("Grand total")
-                }
-                
-                Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                } header: {
-                    Text("Amount per person")
-                }
+    let tipValue = checkAmount * tipSelection
+    let grandTotal = checkAmount + tipValue
+    return grandTotal
+  }
+  
+  var totalPerPerson: Double {
+    let amountPerPerson = grandTotal / peopleTotal
+    return amountPerPerson
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", value: $checkAmount, format:
+              .currency(code: Locale.current.currency?.identifier ?? "USD"))
+          .keyboardType(.decimalPad)
+          .focused($amountIsFocused)
+          
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
             }
-            .navigationTitle("WeSplit")
-            .toolbar {
-                ToolbarItemGroup(placement: .keyboard) {
-                    Spacer()
-                    
-                    Button("Done") {
-                        amountIsFocused = false
-                    }
-                }
-            }
+          }
         }
+        
+        Section {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(tipPercentages, id: \.self) {
+              Text($0, format: .percent)
+            }
+          }
+          .pickerStyle(.segmented)
+        } header: {
+          Text("How much tip do you want to leave?")
+        }
+        
+        Section {
+          Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            .foregroundColor(tipPercentage > 1 ? .primary : .red)
+        } header: {
+          Text("Grand total")
+        }
+        
+        Section {
+          Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        } header: {
+          Text("Amount per person")
+        }
+      }
+      .navigationTitle("WeSplit")
+      .toolbar {
+        ToolbarItemGroup(placement: .keyboard) {
+          Spacer()
+          
+          Button("Done") {
+            amountIsFocused = false
+          }
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
