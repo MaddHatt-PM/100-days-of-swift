fcb5a29d2658a10c0cc77f977e300ab8be7accc5
Patt-Mac <MaddHatt.pm@gmail.com>
2022-08-10 22:22:32 -0400

    Started Project06-Animations

---
+++ Project06-Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Patt Martin on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
