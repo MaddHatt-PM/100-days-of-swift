591e8c557efabf23e1ec9502fb36acaf5fc5e611
Patt-Mac <MaddHatt.pm@gmail.com>
2022-08-10 22:14:58 -0400

    Completed Project05-WordScramble and challenges

--- Project05-WordScramble/WordScramble/ContentView.swift
+++ Project05-WordScramble/WordScramble/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
   @State private var rootWord = ""
   @State private var newWord = ""
   
+  @State private var errorTitle = ""
+  @State private var errorMessage = ""
+  @State private var showingError = false
+  
   var body: some View {
     NavigationStack {
       List {
@@ -30,19 +34,100 @@ struct ContentView: View {
           }
         }
       }
-      .navigationTitle("Word Scramble")
+      .navigationTitle(rootWord)
+      .onAppear(perform: startGame)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button("Restart", action: startGame)
+        }
+      }
+      .alert(errorTitle, isPresented: $showingError) {
+        Button("OK", role: .cancel) { }
+      } message: {
+        Text(errorMessage)
+      }
     }
+  }
+  
+  func startGame() {
+    usedWords = [String]()
+    
+    if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+      if let startWords = try? String(contentsOf: startWordsURL) {
+        let allWords = startWords.components(separatedBy: "\n")
+        rootWord = allWords.randomElement() ?? "restart"
+        return
+      }
+    }
+    
+    fatalError("Could not load start.txt from bundle.")
   }
   
   func addNewWorld() {
     let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-    guard answer.count > 0 else { return }
     
-    // Extra validation to come
+    guard answer != rootWord else {
+      wordError(title: "???", message: "You can't use the same word as an answer")
+      return
+    }
+    
+    guard answer.count > 3 else {
+      wordError(title: "That's too short!", message: "That'd be too easy")
+      return
+    }
+    
+    guard isOriginal(word: answer) else {
+      wordError(title: "Word used already", message: "Be more original")
+      return
+      
+    }
+    
+    guard isPossible(word: answer) else {
+      wordError(title: "Word not possible", message: "You can't spell that word from \(rootWord)")
+      return
+    }
+    
+    guard isReal(word: answer) else {
+      wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+      return
+    }
+    
     withAnimation {
-      usedWords.insert(answer, at: 0)      
+      usedWords.insert(answer, at: 0)
     }
     newWord = ""
+  }
+  
+  func isOriginal(word: String) -> Bool {
+    !usedWords.contains(word)
+  }
+  
+  func isPossible(word: String) -> Bool {
+    var tempWord = rootWord
+    
+    for letter in word {
+      if let pos = tempWord.firstIndex(of: letter) {
+        tempWord.remove(at: pos)
+      } else {
+        return false
+      }
+    }
+    
+    return true
+  }
+  
+  func isReal(word: String) -> Bool {
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    
+    return mispelledRange.location == NSNotFound
+  }
+  
+  func wordError(title: String, message: String) {
+    errorTitle = title
+    errorMessage = message
+    showingError = true
   }
 }
 
